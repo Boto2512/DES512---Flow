ca96959ab521fc9b2bc0901f1f909a339168aca8
breadj <josephdarby15@gmail.com>
2025-06-02 13:39:57 +0100

    PLAYER in Globals and EVENT_PLAYER_MODIFIED Event too

--- Assets/Scripts/Utility/Globals.cs
+++ Assets/Scripts/Utility/Globals.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public static class Globals {
     public static readonly LayerMask DEFAULT_LAYERMASK = Utility.LayerToLayerMask(LayerMask.NameToLayer("Default"));
@@ -7,4 +8,16 @@ public static class Globals {
         
     public static readonly LayerMask STICKY_MASK = DEFAULT_LAYERMASK | GROUND_LAYERMASK;
     public static HashSet<string> STICKY_TAGS = new() { };
+
+    /// <summary>
+    /// Invokes EVENT_PLAYER_MODIFIED when PLAYER is set
+    /// </summary>
+    public static GameObject PLAYER {
+        get { return PLAYER; }
+        set {
+            PLAYER = value;
+            EVENT_PLAYER_MODIFIED.Invoke();
+        }
+    }
+    public static UnityEvent EVENT_PLAYER_MODIFIED = new();
 }
