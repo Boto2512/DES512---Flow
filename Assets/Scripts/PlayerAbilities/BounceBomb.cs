08e200a9b6e450226072d84c97a65385603b66fd
breadj <josephdarby15@gmail.com>
2025-06-02 17:08:07 +0100

    Made typing clearer in Bounce Bomb

--- Assets/Scripts/PlayerAbilities/BounceBomb.cs
+++ Assets/Scripts/PlayerAbilities/BounceBomb.cs
@@ -68,10 +68,10 @@ public class BounceBomb : MonoBehaviour
 
         // sets the momentum of each blast affectable entity to at least the speed minimum of the blast radius it's in, in the direction from itself to the blast origin
         foreach ((IMomentumModifiable entity, bool inStrongBlast) in affectableEntities) {
-            Vector3 momentum = entity.GetMomentum();
             Vector3 projectedPosition = CalculateProjectedEntityPosition(entity);
             Vector3 direction = Vector3.Normalize(projectedPosition - blastOrigin);
 
+            Vector3 momentum = entity.GetMomentum();
             float speedMinimumToUse = inStrongBlast ? strongSpeedMinimum : weakSpeedMinimum;
             Vector3 newMomentum = direction * MathF.Max(momentum.magnitude, speedMinimumToUse);
 
@@ -93,12 +93,12 @@ public class BounceBomb : MonoBehaviour
         var allColliders = new List<(IMomentumModifiable, bool)>();
 
         foreach (var collider in Physics.OverlapSphere(blastOrigin, weakBlastRadius)) {
-            var rb = collider.attachedRigidbody;
+            Rigidbody rb = collider.attachedRigidbody;
             if (rb == null) {
                 continue;
             }
 
-            var imm = rb.gameObject.GetComponent<IMomentumModifiable>();
+            IMomentumModifiable imm = rb.gameObject.GetComponent<IMomentumModifiable>();
             if (imm != null && Physics.Raycast(blastOrigin, collider.ClosestPoint(blastOrigin) - blastOrigin, out RaycastHit outHit, weakBlastRadius)) {
                 allColliders.Add((imm, outHit.distance <= strongBlastRadius));
             }
