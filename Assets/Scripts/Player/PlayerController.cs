9de075bbcb7bc5dab4c97c584f8ac08c4359cda5
SETAREKI LIGAIVIU <2002498@uad.ac.uk>
2025-06-11 15:46:51 +0100

    Viewmodel sway
    +added script that adds view model sway
    +massively tweaked the player prefab
    +re animated the camera in engine
    +new animator controller for the camera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -138,6 +138,7 @@ public class PlayerController : MonoBehaviour, IMomentumModifiable, IDamageable,
     [Space(10)]
     [Header("Animation Controller")]
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator animatorCam;
 
     [Space(10)]
     [Header("Visual Effects")]
@@ -530,7 +531,8 @@ public class PlayerController : MonoBehaviour, IMomentumModifiable, IDamageable,
     private void SpawnBounceBomb() {
         // spawns BounceBomb and 'throws' it via AddForce()
 
-        animator.SetTrigger("HasBombed");
+        animator.SetTrigger("hasBombed");
+        animatorCam.SetTrigger("hasBombed");
 
         bounceBombInstance = Instantiate(bounceBomb, bounceBombSpawnPosition, playerRigidBody.rotation);
         bounceBombInstance.GetComponent<Rigidbody>().AddForce(GetMomentum() + Camera.main.transform.forward * bombThrowPower, ForceMode.VelocityChange);
@@ -542,7 +544,8 @@ public class PlayerController : MonoBehaviour, IMomentumModifiable, IDamageable,
         if (isDetonating)
             return;
 
-        animator.SetTrigger("HasDetonate");
+        animator.SetTrigger("hasDetonate");
+        animatorCam.SetTrigger("hasDetonate");
 
         isDetonating = true;
         this.Invoke(() => {
@@ -589,6 +592,7 @@ public class PlayerController : MonoBehaviour, IMomentumModifiable, IDamageable,
         RaycastHit[] enemies;
 
         animator.SetTrigger("hasAttacked");
+        animatorCam.SetTrigger("hasAttacked");
 
         if (currentStage == 3) {
             // larger aoe && oneshot && vfx
