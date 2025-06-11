a1486c4ebcb0e85f401d7e7cf6a4af659596257d
CONNOR PEGG <1902279@uad.ac.uk>
2025-06-11 17:34:05 +0100

    liquid

---
+++ Assets/VFX/Liquid/LiquidScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LiquidScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RenderBuffer
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
