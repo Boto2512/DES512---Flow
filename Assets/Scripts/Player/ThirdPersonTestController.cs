ebffb683bda64edbb1faffc41bda2c46bf8284f0
breadj <josephdarby15@gmail.com>
2025-06-18 17:51:28 +0100

    Getting on with work. End of day commit+push

--- Assets/Scripts/Player/ThirdPersonTestController.cs
+++ Assets/Scripts/Player/ThirdPersonTestController.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class ThirdPersonTestController : MonoBehaviour {
 
-    private Vector2 movementInput = Vector2.zero;
+    [SerializeField, Range(1, 120)] private int sensivity = 60;
+
+    private Vector2 movementInput;
+    private Vector2 lookInput;
+    private bool attackPressed;
+    private bool throwPressed;
+    private bool jumpInput;
 
     private Rigidbody rb;
 
+    private Transform cameraStand;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rb = this.GetComponent<Rigidbody>();
+        cameraStand = this.transform.Find("CameraStand");
+        if (cameraStand == null) {
+            throw new System.Exception("ThirdPersonTestController requires a CameraStand child GameObject");
+        }
     }
 
     // Update is called once per frame
@@ -19,13 +34,44 @@ public class ThirdPersonTestController : MonoBehaviour {
 
     #region Input
 
-    private void GetInputs() {
-        
+    public void Move(InputAction.CallbackContext context) {
+        movementInput = context.ReadValue<Vector2>();
     }
 
-    private void MovementInput() {
-        movementInput.x = Input.GetAxisRaw("Horizontal");
-        movementInput.y = Input.GetAxisRaw("Vertical");
+    public void Look(InputAction.CallbackContext context) {
+        lookInput = context.ReadValue<Vector2>();
+
+        cameraStand.Rotate(Vector3.up, lookInput.y * 0.02f * sensivity);
+        cameraStand.Rotate(Vector3.right, lookInput.x * 0.02f * sensivity);
+
+        //cameraStand.Rotate(lookInput.y * 0.02f * sensivity, lookInput.x * 0.02f * sensivity, 0f);
+    }
+
+    public void Attack(InputAction.CallbackContext context) {
+        if (context.started) {
+            attackPressed = true;
+        }
+        else if (context.canceled) {
+            attackPressed = false;
+        }
+    }
+
+    public void Throw(InputAction.CallbackContext context) {
+        if (context.started) {
+            throwPressed = true;
+        }
+        else if (context.canceled) {
+            throwPressed = false;
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context) {
+        if (context.started) {
+            jumpInput = true;
+        }
+        else if (context.canceled) {
+            jumpInput = false;
+        }
     }
 
     #endregion Input
