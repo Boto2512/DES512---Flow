9f9e9d6da4b80b99f66fa0b5f83085d364c8180d
CONNOR PEGG <1902279@uad.ac.uk>
2025-06-25 11:41:39 +0100

    jojo

--- Assets/Scripts/Enemy/Projectile/ProjectileHomingOnPlayer.cs
+++ Assets/Scripts/Enemy/Projectile/ProjectileHomingOnPlayer.cs
@@ -5,12 +5,20 @@ public class ProjectileHomingOnPlayer : MonoBehaviour
     [SerializeField] float upwardSpeed = 5;
     [SerializeField] float homingSpeed = 8;
     [SerializeField] float homingDelay = 0.5f;
+    [SerializeField] float totalDistance = 20;
+
+    [SerializeField] Vector3 startingPos;
+    [SerializeField] Vector3 endingPos;
+    private bool isMoving = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
 
-        Invoke(nameof(startHoming), homingDelay);
+        startingPos = transform.position;
+        endingPos = startingPos + Vector3.up * totalDistance;
+
+        //this.Invoke("literally anithing", startHoming, homingDelay);
+        //Invoke(nameof(startHoming), homingDelay);
         
     }
 
@@ -22,6 +30,17 @@ public class ProjectileHomingOnPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endingPos, upwardSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, endingPos) < 0.01f)
+            {
+                isMoving = false;
+            }
+        }
+
+
         
     }
 }
