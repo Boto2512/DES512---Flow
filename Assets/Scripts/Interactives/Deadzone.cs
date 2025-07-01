6b268834d22ca34a186200eecac115fd84581be5
YU-CHE LIN <tommy19981008@gmail.com>
2025-07-01 16:21:50 +0100

    Greyboxing

--- Assets/Scripts/Interactives/Deadzone.cs
+++ Assets/Scripts/Interactives/Deadzone.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 /// <summary>
 /// This script is not an interactive object, it is just for testing.
 /// </summary>
-public class Teleport : MonoBehaviour
+public class Deadzone : MonoBehaviour
 {
-    [SerializeField] private string triggeredTag;
     [SerializeField] private Transform destination;
+
+    private const string playerTag="Player";
+    private const string enemyTag="Enemy";
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.CompareTag(triggeredTag))
+        if (collision.transform.CompareTag(playerTag))
         {
            
             //SetToZero(collision.transform.GetComponent<PlayerController>());
             collision.transform.position = destination.position;
+        }else if (collision.transform.CompareTag(enemyTag))
+        {
+            Destroy(collision.gameObject);
         }
     }
 
