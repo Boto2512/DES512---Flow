fc0f2b2d7cbc1674cf9fd69f1510dbc7ec9c8513
Boto2512 <71253479+Boto2512@users.noreply.github.com>
2025-07-07 11:26:34 +0100

    Globals player setting change - fov changes changed to lerp

--- Assets/Scripts/Player/FirstPersonCameraController.cs
+++ Assets/Scripts/Player/FirstPersonCameraController.cs
@@ -1,4 +1,5 @@
 using Unity.Cinemachine;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -9,6 +10,8 @@ public class FirstPersonCameraController : MonoBehaviour {
     private IMomentumModifiable momentumHolder;
     private SpeedStageThreshold currentThreshold => thresholdHolder.CurrentThreshold;
     private float currentFOV;
+    private float previousTargetFOV;
+    private float fovProgress;
 
 
     #region Cinemachine Game Objects
@@ -34,6 +37,7 @@ public class FirstPersonCameraController : MonoBehaviour {
         UpdateSensitivity();
 
         currentFOV = currentThreshold.FieldOfView;
+        previousTargetFOV = currentFOV;
     }
 
     // Update is called once per frame
@@ -70,10 +74,20 @@ public class FirstPersonCameraController : MonoBehaviour {
     #endregion Visual Effects
 
     private void FovChanges() {
-        float speed = momentumHolder.GetMomentum().magnitude;
 
         float targetFOV = currentThreshold.FieldOfView;
-        currentFOV = Mathf.MoveTowards(currentFOV, targetFOV, Config.fovChangeSpeed * Time.deltaTime);
+        if (targetFOV != previousTargetFOV) { fovProgress = 0; }
+
+        if (currentFOV != targetFOV) {
+
+            currentFOV = Mathf.Lerp(currentFOV, targetFOV, fovProgress);
+            fovProgress += Config.fovChangeSpeed * Time.deltaTime;
+        } 
+        else if ( fovProgress == 1)
+        { 
+            fovProgress = 0.0f;
+        }
         cinemachineCamera.Lens.FieldOfView = currentFOV;
+        previousTargetFOV = targetFOV;
     }
 }
