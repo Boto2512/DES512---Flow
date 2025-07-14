9dd871d02774068aacb2889bae49eaf26b497b7a
Boto2512 <71253479+Boto2512@users.noreply.github.com>
2025-07-14 11:27:43 +0100

    JumpPad

--- Assets/Scripts/Interactives/BoostPadVertical.cs
+++ Assets/Scripts/Interactives/BoostPadVertical.cs
@@ -7,18 +7,22 @@ public class BoostPadVertical : MonoBehaviour
 
     [SerializeField] private float height;
 
-    [SerializeField] private Transform cylinder;
+    private Transform cylinder;
 
     private void Update() {
+        cylinder = transform;
         cylinder.localScale = new Vector3(2, height, 2);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             IMomentumModifiable momentum = other.attachedRigidbody.GetComponent<IMomentumModifiable>();
-            momentum.AddMomentum(transform.up * force);
+            Vector3 playerMomentum = momentum.GetMomentum();
+            Vector3 boostForce = transform.up * force;
+            momentum.SetMomentum(new Vector3 (playerMomentum.x + boostForce.x, boostForce.y, playerMomentum.z + boostForce.z));
+            
         }
     }
 }
