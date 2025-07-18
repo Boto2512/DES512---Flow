238cdec645e0bd4fc7950908ffb3d247ccfd8ece
YANFENG LIU <2310192@uad.ac.uk>
2025-07-18 16:08:56 +0100

    update audio

--- Assets/Scripts/Player/PlayerAttackController.cs
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -61,9 +61,9 @@ public class PlayerAttackController : MonoBehaviour, IDamageable {
     public void Attack() {
         if (attacking)
             return;
-
         attackAnimator.SetTrigger("hasAttacked");
         cameraAnimator.SetTrigger("hasAttacked");
+        AudioManager.instance.Play("PlayerAttackInTheAir");
 
         // TODO: change it so attack range scales with speed too
 
@@ -73,9 +73,9 @@ public class PlayerAttackController : MonoBehaviour, IDamageable {
         //    damageable.TakeDamage(damage);
         //    HitStop.Slow(0.1f, 0.1f).Forget();
         //}
-
         attacking = true;
         hurtbox.gameObject.SetActive(true);
+        
         this.InvokeOverwrite("attack animation playing", () => { attacking = false; parried = false; }, Config.AttackCooldown);
     }
 
@@ -95,6 +95,7 @@ public class PlayerAttackController : MonoBehaviour, IDamageable {
         //HitStop.Stop(0.33f).Forget();
 
         Instantiate(hitVFX, collider.ClosestPoint(hurtbox.transform.position), Quaternion.identity);
+        AudioManager.instance.Play("PlayerAttack");
     }
 
     private void Parry() {
