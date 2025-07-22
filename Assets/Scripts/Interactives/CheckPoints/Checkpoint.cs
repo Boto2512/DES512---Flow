30437a879131caa731d38bffce07a1682d2f5dd0
Boto2512 <71253479+Boto2512@users.noreply.github.com>
2025-07-22 15:36:31 +0100

    Checkpoints

--- Assets/Scripts/Interactives/CheckPoints/Checkpoint.cs
+++ Assets/Scripts/Interactives/CheckPoints/Checkpoint.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    [SerializeField] int childIndex;
+    int childIndex;
     private void OnTriggerEnter(Collider other)
     {
        transform.parent.GetComponent<CheckpointManager>().UpdateCheckpoint(childIndex);
+    }
+
+    public void GetChildIndex(int index)
+    {
+        childIndex = index;
     }
 }
