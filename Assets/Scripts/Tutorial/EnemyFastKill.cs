87a2c3959637bcaf07a1f1a9e056d1f5008adf67
SUMIT BHANDARI <bsumit236@gmail.com>
2025-07-30 15:59:34 +0100

    Final Tutorial Animations in

--- Assets/Scripts/Tutorial/EnemyFastKill.cs
+++ Assets/Scripts/Tutorial/EnemyFastKill.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class EnemyFastKill : ITutorialStep
 {
-    public bool ShouldAutoAdvance() => false;
+    public bool ShouldAutoAdvance() => true;
     private Sprite tutorialGif;
     public Sprite GetGifSprite() => tutorialGif;
     public void SetGifSprite(Sprite sprite) => tutorialGif = sprite;
